6b4af60de1e06690be370942d20d140e05089bbc
unknown <Herschbach.Stefan@DE-MG-15165.ads.local>
2015-05-01 17:51:27 +0200

    Initial commit

---
+++ src/main/java/runningfun/restfulservice/ResourceChat.java
@@ -0,0 +1,28 @@
+package runningfun.restfulservice;
+
+import org.atmosphere.annotation.Broadcast;
+import org.atmosphere.annotation.Suspend;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+@Path("chat")
+@Produces("application/json")
+public class ResourceChat {
+
+    @Suspend
+    @GET
+    public String suspend() {
+        return "hello";
+    }
+
+    @Broadcast(writeEntity = false)
+    @POST
+    public Response broadcast(Message message) {
+        return new Response(message.author, message.message);
+    }
+
+}
---
+++ src/main/java/runningfun/restfulservice/Response.java
@@ -0,0 +1,18 @@
+package runningfun.restfulservice;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+@XmlRootElement
+public class Response {
+public String text;
+public String author;
+public long time;
+public Response(String author, String text) {
+this.author = author;
+this.text = text;
+this.time = new Date().getTime();
+}
+public Response() {
+}
+}
